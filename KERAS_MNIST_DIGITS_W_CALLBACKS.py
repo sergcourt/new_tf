5ae9ba2a963fa7fd952e28562579a8a49f8905c0
sergio <sergiovillani8@gmail.com>
2019-12-17 22:08:54 -0800

    ker_mnist_with_callbacks

---
+++ KERAS_MNIST_DIGITS_W_CALLBACKS.py
@@ -0,0 +1,35 @@
+import tensorflow as tf
+
+
+
+class svCallbacks(tf.keras.callbacks.Callback):
+  def on_epoch_end(self,epochs,logs={}):
+    if (logs.get('acc')>0.99 ):
+      print("\n reached 99% accuracy and canceling the training")
+      self.model.stop_training=True
+
+
+
+mnist = tf.keras.datasets.mnist
+
+(x_train, y_train),(x_test, y_test) = mnist.load_data()
+
+x_train=x_train/255.0
+x_test=x_test/ 255.0
+
+callbacks=svCallbacks()
+
+
+model = tf.keras.models.Sequential([
+                                    tf.keras.layers.Flatten(input_shape=(28,28)),
+                                    tf.keras.layers.Dense(512, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(256, activation=tf.nn.relu),
+                                    tf.keras.layers.Dense(10, activation=tf.nn.softmax)
+])
+
+model.compile(optimizer='adam',
+              loss='sparse_categorical_crossentropy',
+              metrics=['accuracy'])
+
+
+model.fit(x_train, y_train, epochs=10, callbacks=[callbacks])
